0471e2acaa4373ef0623af9ca16a2c7e955672aa
Kirill Lesnykh <kirlesnykh@gmail.com>
2023-04-18 16:33:56 -0700

    lab12 complete

--- driver/handler.js
+++ driver/handler.js
@@ -1,10 +1,14 @@
 'use strict';
 
-const { emitter, eventPool } = require('../eventPool');
+const { io } = require('socket.io-client');
+const SERVER_URL = process.env.PORT || 'http://localhost:3001';
 
-emitter.on(eventPool[0], function(payload) {
-  console.log('DRIVER: picked up ', Object.values(payload)[1]);
-  emitter.emit(eventPool[1], payload);
-  console.log('DRIVER: delivered up ', Object.values(payload)[1]);
-  emitter.emit(eventPool[2], payload);
+let driverSocket = io(SERVER_URL + '/caps');
+
+driverSocket.on('pickup', payload => {
+  driverSocket.emit('join-room', payload.store);
+  console.log('DRIVER: picked up ', payload.orderId);
+  driverSocket.emit('in-transit', payload);
+  console.log('DRIVER: delivered up ', payload.orderId);
+  driverSocket.emit('delivered', payload);
 });
--- vendor/handler.js
+++ vendor/handler.js
@@ -1,16 +1,23 @@
 'use strict';
 
-const { emitter, eventPool } = require('../eventPool');
+'use strict';
 
-emitter.on(eventPool[2], function(payload){
-  console.log(`VENDOR: Thank you for delivering for ${Object.values(payload)[2]}, order ${Object.values(payload)[1]}`);
-});
+const { io } = require('socket.io-client');
+const SERVER_URL = process.env.PORT || 'http://localhost:3001';
 
-emitter.emit(eventPool[0], { 
+let vendorSocket = io(SERVER_URL + '/caps');
+
+let payload = {
   store: '1-206-flowers',
   orderId: 'e3669048-7313-427b-b6cc-74010ca1f8f0',
   customer: 'Jamal Braun',
   address: 'Schmittfort, LA',
+};
+
+vendorSocket.emit('join-room', payload);
+
+vendorSocket.on('delivered', function(payload){
+  console.log(`VENDOR: Thank you for delivering for ${payload.orderId}, order ${payload.orderId}`);
 });
 
-
+vendorSocket.emit('pickup', payload);
